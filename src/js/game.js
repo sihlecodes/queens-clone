a9f8c56f0be3829b4ad76356e1597f13e32ffae6
Siphesihle Mhlongo <sihlecodes@gmail.com>
2024-11-19 14:01:35 +0200

    Improve input system
    Add support for mobile

--- src/js/game.js
+++ src/js/game.js
@@ -4,107 +4,126 @@ import { board } from './board.js';
 const States = {
     NOP: 0,
     MARKING: 1,
-    CLEARING_MARK: 2,
-    CLEARING_QUEEN: 4,
-    DRAGGING: 8,
+    CLEARING: 2,
+    DRAGGING: 4,
 
     overlap: function(state, other) {
         return (state & other) === other;
     }
 }
 
-let state = States.NOP
+let state = States.NOP;
+
+const board_canvas = document.getElementById('board_canvas');
+const marks_canvas = document.getElementById('marks_canvas');
+
+let InputHandler = {
+    state: States.NOP,
+    _event: '',
+
+    change: function(state) {
+        this.state = state;
+    },
+
+    prev: {x: 0, y: 0},
+
+    handle: function (name, event) {
+        let rect = marks_canvas.getBoundingClientRect();
+        let event_x = event.clientX - rect.left;
+        let event_y = event.clientY - rect.top;
+
+        const {x, y} = board.to_relative_position(event_x, event_y);
+        const mark = board.get_mark(x, y);
+
+        switch (name) {
+            case 'mousedown':
+                switch (mark) {
+                    case board.marks.NONE:
+                        this.change(States.MARKING);
+                        break;
+                    case board.marks.BASIC:
+                        this.change(States.CLEARING);
+                        break;
+                    default:
+                        this.change(States.NOP);
+                        break;
+                }
+
+                this.prev = {x: event_x, y: event_y};
+                console.log('set ' + `(${this.prev.x}, ${this.prev.y})`);
+                break;
+            case 'mousemove':
+                console.log('check ' + `(${event_x}, ${event_y})`);
+                if (this.prev.x === event_x && this.prev.y === event_y)
+                    break;
+
+                if (mark === board.marks.QUEEN)
+                    break;
+
+                switch (this.state) {
+                    case States.CLEARING:
+                        board.set_mark(x, y, board.marks.NONE);
+                        break;
+                    case States.MARKING:
+                        board.set_mark(x, y, board.marks.BASIC);
+                        break;
+                }
+
+                render_mark(marks_canvas, board, x, y);
+                break;
+            case 'mouseup':
+                this.change(States.NOP);
+
+                if (this.prev.x !== event_x && this.prev.y !== event_y)
+                    break;
+
+                board.cycle_mark(x, y);
+                render_mark(marks_canvas, board, x, y);
+                break;
+            case 'mouseleave':
+                this.change(States.NOP);
+                break;
+        }
+    },
+};
+
 
 const Game = {
-    start: function() {
-        const board_canvas = document.getElementById("board_canvas");
-        const marks_canvas = document.getElementById("marks_canvas");
-
+    start: function () {
         board.init();
-        // board.set_mark(0, 0, board.marks.QUEEN);
-        // render_mark(marks_canvas, board, 0, 0);
 
         render(board_canvas, board, color_map);
         this.handle_input(marks_canvas, board);
     },
 
-    handle_input: function(element, board) {
-        element.addEventListener("mousedown", function(e) {
-            const {x, y} = board.to_relative_position(e.offsetX, e.offsetY);
-            const mark = board.get_mark(x, y);
-
-            switch (mark) {
-                case board.marks.NONE:
-                    state = States.MARKING;
-                    break;
-                case board.marks.BASIC:
-                    state = States.CLEARING_MARK;
-                    break;
-                default:
-                    state = States.CLEARING_QUEEN;
-                    break;
-            }
+    handle_input: function (element, board) {
+        element.addEventListener('mousedown', function (e) {
+            InputHandler.handle('mousedown', e);
         });
 
-        element.addEventListener("dblclick", function(e) {
-            const {x, y} = board.to_relative_position(e.offsetX, e.offsetY);
-
-            board.set_mark(x, y, board.marks.QUEEN);
-            render_mark(element, board, x, y);
-
-            state = States.NOP;
+        element.addEventListener('touchstart', function (e) {
+            InputHandler.handle('mousedown', e.touches[0])
+        });
+        element.addEventListener('touchmove', function(e) {
+            InputHandler.handle('mousemove', e.touches[0])
         });
 
-        element.addEventListener("mouseleave", function(e) {
-            state = States.NOP;
+        element.addEventListener('touchend', function (e) {
+            InputHandler.handle('mouseup', e)
         });
-
-        element.addEventListener("mouseup", function(e) {
-            const {x, y} = board.to_relative_position(e.offsetX, e.offsetY);
-
-            if (States.overlap(state, States.DRAGGING)) {
-                state = States.NOP;
-                return;
-            }
-
-            switch (state) {
-                case States.CLEARING_MARK:
-                case States.CLEARING_QUEEN:
-                    board.cycle_mark(x, y);
-                    break;
-
-                case States.MARKING:
-                    board.set_mark(x, y, board.marks.BASIC);
-                    break;
-            }
-
-            render_mark(element, board, x, y);
-            state = States.NOP;
+        element.addEventListener('mouseleave', function (e) {
+            InputHandler.handle('mouseleave', e);
         });
-
-        element.addEventListener("mousemove", function(e) {
-            const {x, y} = board.to_relative_position(e.offsetX, e.offsetY);
-
-            if (state === States.NOP)
-                return;
-
-            state |= States.DRAGGING;
-
-            if (!board.get_mark(x, y) && States.overlap(state, States.MARKING)) {
-                board.set_mark(x, y, board.marks.BASIC);
-            }
-            else if (States.overlap(state, States.CLEARING_MARK)) {
-                if (board.get_mark(x, y) !== board.marks.QUEEN)
-                    board.set_mark(x, y, board.marks.NONE);
-            }
-
-            if (state != States.NOP)
-                render_mark(element, board, x, y);
+        element.addEventListener('mouseup', function (e) {
+            InputHandler.handle('mouseup', e);
+        });
+        element.addEventListener('mousemove', function (e) {
+            InputHandler.handle('mousemove', e);
         });
     },
 
-    debug: function(text) {
-        const debug = document.getElementById("debug");
+    debug: function (text) {
+        const debug = document.getElementById('debug');
         debug.innerText = text;
     }
 }
