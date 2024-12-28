8d39d34b2be89aa2f4d5db5aef3e426a786e2f26
Siphesihle Mhlongo <sihlecodes@gmail.com>
2024-12-28 18:50:48 +0200

    Fix Queens validator bugs (#7)

--- public/js/validator.js
+++ public/js/validator.js
@@ -2,7 +2,8 @@ import { QueenCollection } from "./collections.js";
 
 export class QueensValidator {
     constructor() {
-        this._queens = new QueenCollection();
+        this.queens = new QueenCollection();
+        this.is_valid = true;
 
         this.handlers = {
             on_invalid_proximity: undefined,
@@ -12,18 +13,6 @@ export class QueensValidator {
             on_remove_queen: undefined,
             on_valid_queens: undefined,
         };
-    }
-
-    revalidate() {
-        let is_valid = false;
-
-        for (let i = 0; i < this._queens.length(); i++) {
-            const {column, row, color} = this._queens.at(i);
-
-            is_valid |= this.validate(this._queens.slice(i+1), column, row, color);
-        }
-
-        return is_valid;
     }
 
     get_nearby_queen_positions(queens, x, y) {
@@ -43,14 +32,26 @@ export class QueensValidator {
         return coords;
     }
 
+    revalidate() {
+        let is_valid = true;
+
+        for (let i = 0; i < this.queens.length(); i++) {
+            const {column, row, color} = this.queens.at(i);
+
+            is_valid &= this.validate(this.queens.slice(i+1), column, row, color);
+        }
+
+        return is_valid;
+    }
+
     validate(queens, x, y, color) {
         const queen_positions = this.get_nearby_queen_positions(queens, x, y);
 
-        let surrounded = queen_positions.length > 1;
+        const surrounded = queen_positions.length > 1;
         const includes_color = queens.includes_color(color);
         const includes_column = queens.includes_column(x);
         const includes_row = queens.includes_row(y);
-        const is_valid = !(surrounded | includes_color | includes_column | includes_row);
+        const is_valid = !(surrounded || includes_color || includes_column || includes_row);
 
         if (surrounded)
             this.handlers.on_invalid_proximity?.(queen_positions);
@@ -69,18 +70,19 @@ export class QueensValidator {
 
     remove_and_revalidate(x, y, color) {
         this.handlers.on_remove_queen?.();
-        this._queens.remove(x, y, color);
+        this.queens.remove(x, y, color);
 
-        if(this.revalidate())
-            this.handlers.on_valid_queens?.(this._queens.length());
+        this.is_valid = this.revalidate();
+
+        if (this.is_valid)
+            this.handlers.on_valid_queens?.(this.queens.length());
     }
 
     push_and_validate(x, y, color) {
-        const is_valid = this.validate(this._queens, x, y, color);
+        this.is_valid &= this.validate(this.queens, x, y, color);
+        this.queens.push(x, y, color);
 
-        this._queens.push(x, y, color);
-
-        if (is_valid)
-            this.handlers.on_valid_queens?.(this._queens.length());
+        if (this.is_valid)
+            this.handlers.on_valid_queens?.(this.queens.length());
     }
 }
